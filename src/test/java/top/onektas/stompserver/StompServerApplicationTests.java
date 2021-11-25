7f59f33010f04d88aad74937bfb8500a59f512c5
Onektas <2187906159@qq.com>
2021-11-25 13:47:26 +0800

    0

---
+++ src/test/java/top/onektas/stompserver/StompServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.onektas.stompserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
