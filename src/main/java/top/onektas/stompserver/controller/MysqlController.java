04b1229310e60461f3e4097baaf985c9f2d8af88
Onektas <2187906159@qq.com>
2021-12-08 16:05:22 +0800

    重构

--- src/main/java/top/onektas/stompserver/controller/MysqlController.java
+++ src/main/java/top/onektas/stompserver/controller/MysqlController.java
@@ -1,8 +1,11 @@
 package top.onektas.stompserver.controller;
 
+import lombok.SneakyThrows;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import top.onektas.stompserver.tools.ResultSetTranslator;
 
 import java.sql.*;
+import java.util.Properties;
 
 /**
  * Mysql查询类
@@ -13,17 +16,59 @@ public class MysqlController {
 
     // MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/work";
+    private static String DB_URL = MysqlController.db_url();
 
     // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
     //static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     //static final String DB_URL = "jdbc:mysql://localhost:3306/work?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
-    // 数据库的用户名与密码，需要根据自己的设置
-    static final String USER = "root";
-    static final String PASS = "1234";
+    // 数据库的用户名与密码以及SQL语句，在mysql.properties中修改
+    private static String USER = MysqlController.user();
+    private static String PASS = MysqlController.pass();
+    private static String SQL = MysqlController.sql();
 
-    public String mysqlConntroller(String zhengzhuang) {
+    @SneakyThrows
+    private static String db_url() {
+        new Properties();
+        Properties props;
+        props = PropertiesLoaderUtils.loadAllProperties("mysql.properties");
+        String DB_URL = (String) props.get("DB_URL");
+
+        return DB_URL;
+    }
+
+    @SneakyThrows
+    private static String user() {
+        new Properties();
+        Properties props;
+        props = PropertiesLoaderUtils.loadAllProperties("mysql.properties");
+        String USER = (String) props.get("USER");
+
+        return USER;
+    }
+
+    @SneakyThrows
+    private static String pass() {
+        new Properties();
+        Properties props;
+        props = PropertiesLoaderUtils.loadAllProperties("mysql.properties");
+        String PASS = (String) props.get("PASS");
+
+        return PASS;
+    }
+
+    @SneakyThrows
+    private static String sql() {
+        new Properties();
+        Properties props;
+        props = PropertiesLoaderUtils.loadAllProperties("mysql.properties");
+        String SQL = (String) props.get("SQL");
+
+        return SQL;
+    }
+
+
+    public String mysqlConntroller(String data) {
         Connection conn = null;
         Statement stmt = null;
         String result = null;
@@ -40,7 +85,7 @@ public class MysqlController {
             System.out.println("查询数据库数据...");
             stmt = conn.createStatement();
             String sql;
-            sql = "SELECT buwei,xuewei,bingzheng FROM xuewei WHERE bingzheng LIKE '%" + zhengzhuang + "%'";
+            sql = SQL + data + "%'";
             ResultSet rs = stmt.executeQuery(sql);
 
             // 将查询结果转换为String
